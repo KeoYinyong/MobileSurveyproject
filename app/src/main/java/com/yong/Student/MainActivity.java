d849c9ca5a7e91477397c1c347c3274345186e67
KeoYinyong <yongkeo9@gmail.com>
2024-02-08 11:45:34 +0700

    connect firebase login signup

--- app/src/main/java/com/yong/Student/MainActivity.java
+++ app/src/main/java/com/yong/Student/MainActivity.java
@@ -1,37 +1,35 @@
 package com.yong.Student;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button signup = (Button) findViewById(R.id.btn_signup);
-        Button login = (Button) findViewById(R.id.btn_login);
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SignUp.class);
-                startActivityForResult(intent, 1);
 
-            }
-        });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), mainMenu.class);
-                startActivityForResult(intent, 1);
-
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Menu");
+        setSupportActionBar(toolbar);
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
